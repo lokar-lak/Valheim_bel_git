39807d16c9610956c8b7e2bf884cc9d4dc5a8c90
łokar <lokar.lakalizacja@gmail.com>
2025-08-23 21:21:41 +0200

    New translations csvtotxt.py (Ukrainian)

--- uk/CSVtoTXT.py
+++ uk/CSVtoTXT.py
@@ -8,21 +8,35 @@ with open(input_file, "r", encoding="utf-8") as infile:
 
 # Замена першых трох радкоў
 if len(lines) >= 3:
-    lines[0] = '0 TextAsset Base\n'
-    lines[1] = ' 1 string m_Name = "localization"\n'
-    lines[2] = ' 1 string m_Script = """\n'
+    lines[0] = '0 TextAsset Base
+'
+    lines[1] = ' 1 string m_Name = "localization"
+'
+    lines[2] = ' 1 string m_Script = """
+'
 
-# Аб’яднанне ў адзіны радок і замена ўсіх новых радкоў на \n
+# Аб’яднанне ў адзіны радок і замена ўсіх новых радкоў на 
+
 content = ''.join(lines)
-converted = content.replace('\n', '\\n')
+converted = content.replace('
+', '\
+')
 
-# Зваротная замена для тых першых трох, каб яны былі як трэба (без \\n)
-converted = converted.replace('0 TextAsset Base\\n', '0 TextAsset Base\n', 1)
-converted = converted.replace(' 1 string m_Name = "localization"\\n', ' 1 string m_Name = "localization"\n', 1)
-converted = converted.replace(' 1 string m_Script = """\\n', ' 1 string m_Script = """\n', 1)
+# Зваротная замена для тых першых трох, каб яны былі як трэба (без \
+)
+converted = converted.replace('0 TextAsset Base\
+', '0 TextAsset Base
+', 1)
+converted = converted.replace(' 1 string m_Name = "localization"\
+', ' 1 string m_Name = "localization"
+', 1)
+converted = converted.replace(' 1 string m_Script = """\
+', ' 1 string m_Script = """
+', 1)
 
 # Дадаем закрываючы """ у канцы
-converted += '\n"""'
+converted += '
+"""'
 
 # Запіс у файл
 with open(output_file, "w", encoding="utf-8") as outfile:
